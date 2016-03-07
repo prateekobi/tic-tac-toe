16e7eae3bc6c14284ac2710dcdca2bd21a8aba28
Prateek Obireddy <Prateek@Prateeks-MacBook-Air.local>
2016-03-08 10:43:30 +1100

    updated files

---
+++ projectzero.js
@@ -0,0 +1,8 @@
+/*winning combinations rows ([1,2,3],[3,2,1],[4,5,6],[6,5,4],[7,8,9],[9,8,7]) - rows
+winning combinations col  ([1,4,7].[7,4,1],[2,5,8],[8,5,2],[3,6,9],[9,6,3]) - col
+winning combinations dia  ([1,5,9],[9,5,1],[3,5,7],[7,5,3]) - dia*/
+
+$(document).ready(function(){
+var player1 = "";
+var player2 = "";
+var turn = "";
